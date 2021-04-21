568cd976df89989d42d14fc9f8315a309a20a789
raihanwebmaster <raihanemon2015@gmail.com>
2021-04-21 06:30:49 +0200

    okay

---
+++ src/routes/admin/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { signup, signin, requireSignin } = require("../../controllers/admin/auth");
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require("../../validators/auth");
+
+router.post("/admin/signup", validateSignupRequest, isRequestValidated ,signup);
+router.post("/admin/signin",validateSigninRequest, isRequestValidated, signin);
+// router.post('/profile', requireSignin, (req, res) => {
+//     res.status(200).json({user:'profile'});
+// })
+
+module.exports = router;
---
+++ src/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { signup, signin, requireSignin } = require("../controllers/auth");
+const { validateSignupRequest, isRequestValidated, validateSigninRequest} = require("../validators/auth");
+
+const router = express.Router();
+router.post("/signup", validateSignupRequest, isRequestValidated ,signup);
+router.post("/signin", validateSigninRequest, isRequestValidated, signin);
+// router.post('/profile', requireSignin, (req, res) => {
+//     res.status(200).json({user:'profile'});
+// })
+
+module.exports = router;
