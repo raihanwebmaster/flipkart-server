dd53fb51847b11fb7645a29a04bd9cf3fc337a1c
raihanwebmaster <raihanemon2015@gmail.com>
2021-07-26 20:05:38 +0200

    user login and product

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -43,7 +43,7 @@ exports.signin = (req, res) => {
   User.findOne({ email: req.body.email }).exec((error, user) => {
     if (error) return res.status(400).json({ error });
     if (user) {
-      if (user.authenticate(req.body.password)) {
+      if (user.authenticate(req.body.password) && user.role === "user") {
         const token = jwt.sign({ _id: user._id , role: user.role}, process.env.JWT_SECRET, {
           expiresIn: "1h",
         });
@@ -54,7 +54,7 @@ exports.signin = (req, res) => {
         });
       }else{
         res.status(404).json({
-          message: "Invalid Password",
+          message: "Somethings went wrong",
         })
       }
     } else {
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const { requireSignin, adminMiddleware } = require("../common-middleware");
-const { createProduct, getProductsBySlug } = require("../controllers/product");
+const { createProduct, getProductsBySlug, getProductDetailsById } = require("../controllers/product");
 const multer = require("multer");
 const router = express.Router();
 const shortid = require("shortid");
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function(res, file, cb){
-        cb(null, path.join(path.dirname(__dirname), 'uploads'))
-    },
-    filename: function(res, file, cb){
-        cb( null, shortid.generate() + '-' + file.originalname)
-    }
-})
+  destination: function (res, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: function (res, file, cb) {
+    cb(null, shortid.generate() + "-" + file.originalname);
+  },
+});
 
-const upload = multer({ storage})
+const upload = multer({ storage });
 
-router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture') , createProduct );
-router.get('/products/:slug', getProductsBySlug);
+router.post(
+  "/product/create",
+  requireSignin,
+  adminMiddleware,
+  upload.array("productPicture"),
+  createProduct
+);
+router.get("/products/:slug", getProductsBySlug);
 
 // router.get('/category/getcategory', getCategory );
+router.get("/product/:productId", getProductDetailsById);
 
 module.exports = router;
